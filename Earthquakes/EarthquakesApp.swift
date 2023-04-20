ec3c30d97e71841921e1d2ea7e670c7e1f2d124e
thanhthan6 <nghia2862002@gmail.com>
2023-04-20 10:57:12 +0700

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Macbook on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
